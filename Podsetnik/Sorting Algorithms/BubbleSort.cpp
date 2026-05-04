5e7df4883c56a098d31646be109668190e801cfd
velickovicv <160411101+velickovicv@users.noreply.github.com>
2026-05-04 16:40:09 +0200

    Create BubbleSort.cpp

---
+++ Podsetnik/Sorting Algorithms/BubbleSort.cpp
@@ -0,0 +1,12 @@
+int i, j, temp;
+
+for (i = 0; i < n - 1; i++) {
+    for (j = 0; j < n - i - 1; j++) {
+
+        if (A[j] > A[j + 1]) {
+            temp = A[j];
+            A[j] = A[j + 1];
+            A[j + 1] = temp;
+        }
+    }
+}
